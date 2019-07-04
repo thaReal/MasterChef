d6eb914abda8efeb9bf451141fb6e833adb7d85c
thaReal <daniel.ruland@gmail.com>
2019-07-04 15:40:58 -0400

    updates on candy box problem

--- codeforces/round_570/candybox.py
+++ codeforces/round_570/candybox.py
@@ -3,17 +3,61 @@
 # Computer Game
 
 from collections import defaultdict
-
+'''
+# too slow
 def candysort(n, an):
 	box = defaultdict(int)
 	for ai in an:
 		box[ai] += 1
 	
+	candies = box.values()
+	candies.sort(reverse=True)
+
+	for i in range(len(candies)):
+		if i == 0:
+			val = candies[i]
+			continue
+			
+		if val == 1:
+			candies = candies[:i]
+			break
+		
+		if candies[i] >= val:
+			candies[i] = val - 1
+			val = candies[i]
+			continue
+		
+		val = candies[i]
+
+	return sum(candies)
+'''
+
+def candysort(n, an):
+	box = list()
+	for i in range(n):
+		box[i] = 0
+		
+	for i in an:
+		box[an] += 1
+	
+	candy = list()
+	for i in range(len(box) + 1):
+		candy[i] = 0
+	
+	for i in range(n):
+		if box[i] != 0:
+			candy[box[i]] += 1
+	
+	sol = 0
+	for i in range(n, 0, -1):
+		 if candy[i] != 0:
+		 	sol
+	
 
 
 #Main
 q = int(raw_input())
-for query in range(q)
+for query in range(q):
 	n = int(raw_input())
 	an = [int(x) for x in raw_input().split(" ")]
 	

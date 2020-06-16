ef51cbeab70c9a7eddbbe8090c9e1f090008fcc8
Danny <daniel.ruland@gmail.com>
2020-06-16 10:27:20 -0400

    [Tools] Primes
    Updates to Seive of Erasthones. I believe just added driver code and some
    boundary case checking.

--- tools/primes/seive_of_eratosthenes.py
+++ tools/primes/seive_of_eratosthenes.py
@@ -1,12 +1,19 @@
-#!/usr/bin/python
-# Python program to print all primes smaller than or equal to 
-# n using Sieve of Eratosthenes 
+#!/usr/bin/python3
 
-def SieveOfEratosthenes(n): 
-	
-	# Create a boolean array "prime[0..n]" and initialize 
-	# all entries it as true. A value in prime[i] will 
-	# finally be false if i is Not a prime, else true. 
+# seive_of_eratosthenes.py: Danny Ruland 
+# Description: Seive of Eratosthenes implementation to calculate all the prime 
+# numbers less than or equal to n 
+
+'''
+Create a boolean array "prime[0..n]" and initialize 
+all entries it as true. A value in prime[i] will 
+finally be false if i is Not a prime, else true. 
+'''
+
+def SieveOfEratosthenes(n, showNums=True, lessThan=False):
+	if n == 0:
+		return 0
+		
 	prime = [True for i in range(n + 1)] 
 	p = 2
 	while (p * p <= n): 
@@ -18,17 +25,29 @@ def SieveOfEratosthenes(n):
 			for i in range(p * 2, n + 1, p): 
 				prime[i] = False
 		p += 1
+		
 	prime[0]= False
 	prime[1]= False
-	# Print all prime numbers 
-	for p in range(n + 1): 
+	
+	# Print all prime numbers
+	primes = []
+	for p in range(n): 
 		if prime[p]: 
-			print p, 
+			primes.append(p)
+	
+	return primes
+			
+			
 
 # driver program 
 if __name__=='__main__': 
-	n = 999999733
-	print "Following are the prime numbers smaller", 
-	print "than or equal to", n 
-	SieveOfEratosthenes(n) 
+	# This is disgusting...
+	# DELETE AND REDO
+	
+	n = 13
+	print ("Following are the prime numbers smaller") 
+	print ("than or equal to {}".format(n)) 
+	primes = SieveOfEratosthenes(n)
+	print (primes)
+	#n = 999999733 #too big
 
